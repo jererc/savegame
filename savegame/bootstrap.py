13792458f07e5789905f916841239ec70518b4a5
jererc <jererc@gmail.com>
2024-11-12 14:45:31 +0400

    update

--- savegame/bootstrap.py
+++ savegame/bootstrap.py
@@ -6,7 +6,6 @@ from svcutils import Bootstrapper
 Bootstrapper(
     script_path=os.path.join(os.path.dirname(os.path.realpath(__file__)),
         'savegame.py'),
-    crontab_schedule='*/2 * * * *',
     linux_args=['save', '--task'],
     windows_args=['save', '--daemon'],
 ).run()
