43557f2318512e0debed5ccc656e35f633e3275c
jererc <jererc@gmail.com>
2024-11-13 16:34:40 +0400

    update

--- savegame/savegame.py
+++ savegame/savegame.py
@@ -25,7 +25,7 @@ from svcutils import Notifier, RunFile, Service, get_file_mtime, get_logger
 SAVES = []
 RUN_DELTA = 30 * 60
 FORCE_RUN_DELTA = 90 * 60
-MIN_RUNNING_TIME = 300
+MIN_RUNTIME = 300
 MAX_CPU_PERCENT = 10
 RETRY_DELTA = 2 * 3600
 RETENTION_DELTA = 7 * 24 * 3600
@@ -887,7 +887,7 @@ def main():
             work_path=WORK_PATH,
             run_delta=RUN_DELTA,
             force_run_delta=FORCE_RUN_DELTA,
-            min_running_time=MIN_RUNNING_TIME,
+            min_runtime=MIN_RUNTIME,
             requires_online=False,
             max_cpu_percent=MAX_CPU_PERCENT,
             loop_delay=60,
