359466112473975a5acc64d161d642dde9b98ced
jererc <jererc@gmail.com>
2024-11-15 13:46:03 +0400

    update

--- savegame/svcutils.py
+++ savegame/svcutils.py
@@ -193,7 +193,6 @@ class Service:
         return True
 
     def _must_run(self):
-        self.tracker.update()
         run_ts = self.run_file.get_ts()
         now_ts = time.time()
         if self.force_run_delta and now_ts > run_ts + self.force_run_delta:
@@ -202,8 +201,9 @@ class Service:
             return self.tracker.check() and self._check_cpu_usage()
         return False
 
-    def _run_once(self):
+    def _attempt_run(self):
         try:
+            self.tracker.update()
             if self._must_run():
                 try:
                     self.callable()
@@ -215,7 +215,7 @@ class Service:
     def run_once(self):
         @with_lockfile(self.work_path)
         def run():
-            self._run_once()
+            self._attempt_run()
 
         run()
 
@@ -223,7 +223,7 @@ class Service:
         @with_lockfile(self.work_path)
         def run():
             while True:
-                self._run_once()
+                self._attempt_run()
                 logger.debug(f'sleeping for {self.loop_delay} seconds')
                 time.sleep(self.loop_delay)
 
