5d0509f0cd9339430a5c7f6e61e0cbdb48d24318
jererc <jererc@gmail.com>
2024-11-17 14:52:07 +0400

    update

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -1,14 +1,22 @@
 import os
 import urllib.request
 
-url = 'https://raw.githubusercontent.com/jererc/svcutils/refs/heads/main/svcutils/bootstrap.py'
-response = urllib.request.urlopen(url)
-code = response.read().decode('utf-8')
-exec(code)
+def get(url):
+    return urllib.request.urlopen(url).read().decode('utf-8')
+
+bootstrap_url = 'https://raw.githubusercontent.com/jererc/svcutils/refs/heads/main/svcutils/bootstrap.py'
+target_url = 'https://raw.githubusercontent.com/jererc/savegame/refs/heads/main/scripts/run.py'
+
+exec(get(bootstrap_url))
+target_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+    os.path.basename(target_url))
+content = get(target_url)
+with open(target_path, 'w') as fd:
+    fd.write(content)
+
 Bootstrapper(
     name='savegame',
-    target_path=os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), 'run.py'),
+    target_path=target_path,
     force_reinstall=True,
     requires=[
         'git+https://github.com/jererc/savegame.git',
