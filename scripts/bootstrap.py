05b07979b937778441aafce2bac8bf7aa06cfd07
jererc <jererc@gmail.com>
2024-11-17 13:43:25 +0400

    update

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -7,12 +7,11 @@ code = response.read().decode('utf-8')
 exec(code)
 Bootstrapper(
     name='savegame',
-    script_path=os.path.join(os.path.dirname(
+    target_path=os.path.join(os.path.dirname(
         os.path.realpath(__file__)), 'run.py'),
     force_reinstall=True,
     requires=[
         'git+https://github.com/jererc/savegame.git',
     ],
-    linux_args=['save', '--task'],
-    windows_args=['save', '--task'],
+    args=['save', '--task'],
 ).run()
