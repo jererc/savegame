6c05837ec6036937ec7e66745f504dbbf6b84732
jererc <jererc@gmail.com>
2024-11-20 11:55:03 +0400

    update

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -5,8 +5,7 @@ url = 'https://raw.githubusercontent.com/jererc/svcutils/refs/heads/main/svcutil
 exec(urllib.request.urlopen(url).read().decode('utf-8'))
 Bootstrapper(
     name='savegame',
-    script_module='savegame.main',
-    script_args=['-p', os.getcwd(), 'save', '--task'],
+    cmd_args=['savegame.main', '-p', os.getcwd(), 'save', '--task'],
     install_requires=[
         # 'git+https://github.com/jererc/savegame.git',
         'savegame @ https://github.com/jererc/savegame/archive/refs/heads/main.zip',
