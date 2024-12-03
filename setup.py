d6ee972e3f4f6be3bfcaa815b7e227f9a4b6fda0
jererc <jererc@gmail.com>
2024-12-03 17:39:55 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='savegame',
-    version='2024.12.02.103535',
+    version='2024.12.03.173954',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/savegame',
