11af2a6a2918611bd0f558fe23a6ad184d0f2789
jererc <jererc@gmail.com>
2024-12-05 10:37:43 +0400

    do epic things

--- bootstrap/user_settings.py
+++ bootstrap/user_settings.py
@@ -1,66 +1,54 @@
 import os
 
 DST_PATH = os.path.join('~', 'MEGA', 'data')
-GOOGLE_CREDS = os.path.join(os.path.expanduser('~'), 'google_creds.json')
-SAVES = [
-    {
-        'src_paths': [
-            r'~\Documents\Battlefield 2042',
-            [
-                r'C:\Users\Public\Documents\*',
-                [],
-                [r'*\desktop.ini', r'*\My Music*', r'*\My Pictures*', r'*\My Videos*'],
-            ],
-        ],
-    },
-    {
-        'src_paths': [
-            r'~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\shortcuts',
-            r'~\AppData\Roaming\Sublime Text 3',
-            r'C:\Program Files (x86)\Steam\steamapps\common\Steam Controller Configs',
-        ],
-        'run_delta': 2 * 3600,
-    },
-    {
-        'src_paths': [
-            '~/.bash_aliases',
-            '~/.config/sublime-text',
-            '~/.gitconfig',
-            '~/.local/share/applications',
-            [
-                '~/.ssh',
-                [],
-                ['*/id_ed25519'],
-            ],
-            [
-                '~/data/code',
-                [],
-                ['*/.git*', '*/venv*', '*/__pycache__*', '*.pyc', '*/node_modules*'],
-            ],
-        ],
-        'run_delta': 2 * 3600,
-    },
-    {
-        'saver_id': 'google_drive_export',
-        'os_name': 'nt',
-        'run_delta': 12 * 3600,
-    },
-    {
-        'saver_id': 'google_contacts_export',
-        'os_name': 'nt',
-        'run_delta': 12 * 3600,
-    },
-    {
-        'saver_id': 'bookmarks_export',
-        'run_delta': 12 * 3600,
-    },
-    {
-        'src_paths': [
-            os.path.join('~', 'MEGA', 'data', 'env'),
-        ],
-        'dst_path': r'~\OneDrive\data',
-        'os_name': 'nt',
-        'run_delta': 12 * 3600,
-        'loadable': False,
-    },
-]
+# SAVES = [
+#     {
+#         'src_paths': [
+#             r'~\Documents\Battlefield 2042',
+#             [
+#                 r'C:\Users\Public\Documents\*',
+#                 [],
+#                 [r'*\desktop.ini', r'*\My Music*', r'*\My Pictures*', r'*\My Videos*'],
+#             ],
+#         ],
+#     },
+#     {
+#         'src_paths': [
+#             r'~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\shortcuts',
+#             r'~\AppData\Roaming\Sublime Text 3',
+#             r'C:\Program Files (x86)\Steam\steamapps\common\Steam Controller Configs',
+#         ],
+#         'run_delta': 2 * 3600,
+#     },
+#     {
+#         'src_paths': [
+#             '~/.bash_aliases',
+#             '~/.config/sublime-text',
+#             '~/.gitconfig',
+#             '~/.local/share/applications',
+#             [
+#                 '~/.ssh',
+#                 [],
+#                 ['*/id_ed25519'],
+#             ],
+#             [
+#                 '~/data/code',
+#                 [],
+#                 ['*/.git*', '*/venv*', '*/__pycache__*', '*.pyc', '*/node_modules*'],
+#             ],
+#         ],
+#         'run_delta': 2 * 3600,
+#     },
+#     {
+#         'saver_id': 'google_drive_export',
+#         'run_delta': 12 * 3600,
+#     },
+#     {
+#         'saver_id': 'google_contacts_export',
+#         'run_delta': 12 * 3600,
+#     },
+#     {
+#         'saver_id': 'bookmarks_export',
+#         'run_delta': 12 * 3600,
+#     },
+# ]
