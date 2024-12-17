785df294fdfa4e348df217e2e4c40af4fe8e153c
jererc <jererc@gmail.com>
2024-12-17 16:52:58 +0400

    do epic things

--- savegame/savers/bookmarks.py
+++ savegame/savers/bookmarks.py
@@ -71,13 +71,13 @@ class BookmarksHandler:
             with open(file, encoding='utf-8') as fd:
                 data = json.load(fd)
             self._set_date_last_used_to_zero(data)
-            dirname = os.path.basename(profile_path)
+            rel_path = os.path.relpath(profile_path, data_dir)
             yield {
-                'path': os.path.join(dirname, self.filename),
+                'path': os.path.join(rel_path, self.filename),
                 'content': json.dumps(data, sort_keys=True, indent=4),
             }
             yield {
-                'path': os.path.join(dirname, f'{self.filename}.html'),
+                'path': os.path.join(rel_path, f'{self.filename}.html'),
                 'content': self._to_html(data),
             }
 
