d4f5305c26126bb475d4fba6f56a0c145695b7bf
jererc <jererc@gmail.com>
2025-05-25 12:15:40 +0400

    do epic things

--- savegame/savers/local.py
+++ savegame/savers/local.py
@@ -1,5 +1,7 @@
 import os
+from pprint import pformat
 import shutil
+import time
 
 from savegame import logger
 from savegame.lib import HOSTNAME, REF_FILENAME, check_patterns, get_file_hash
@@ -32,7 +34,19 @@ class LocalSaver(BaseSaver):
         return self.src, set()
 
     def do_run(self):
+        debug_data = {
+            'src': self.src,
+            'dst': self.dst,
+            'hash_duration': 0,
+            'copy_duration': 0,
+            'copy_count': 0,
+            'file_count': 0,
+            'list_files_duration': 0,
+        }
+        t0 = time.time()
         src, src_files = self._get_src_and_files()
+        debug_data['file_count'] = len(src_files)
+        debug_data['list_files_duration'] = time.time() - t0
         self.report.add('files', self.src, src_files)
         self.ref.src = src
         self.ref.files = {}
@@ -40,14 +54,21 @@ class LocalSaver(BaseSaver):
             rel_path = os.path.relpath(src_file, src)
             dst_file = os.path.join(self.dst, rel_path)
             self.dst_paths.add(dst_file)
+            t0 = time.time()
             src_hash = get_file_hash(src_file)
             dst_hash = get_file_hash(dst_file)
+            debug_data['hash_duration'] += time.time() - t0
             try:
                 if src_hash != dst_hash:
                     os.makedirs(os.path.dirname(dst_file), exist_ok=True)
+                    t0 = time.time()
                     shutil.copy2(src_file, dst_file)
+                    debug_data['copy_duration'] += time.time() - t0
+                    debug_data['copy_count'] += 1
                     self.report.add('saved', self.src, src_file)
                 self.ref.files[rel_path] = src_hash
             except Exception:
                 self.report.add('failed', self.src, src_file)
                 logger.exception(f'failed to save {src_file}')
+        if len(src_files) > 100:
+            logger.info(f'debug data:\n{pformat(debug_data)}')
