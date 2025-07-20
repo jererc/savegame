a35ccf262608730d99ed498533ffd287820d3aab
jererc <jererc@gmail.com>
2025-07-20 14:47:23 +0400

    do epic things

--- savegame/__init__.py
+++ savegame/__init__.py
@@ -1,8 +1,5 @@
-import logging
-
 from svcutils.service import get_work_dir, setup_logging
 
 NAME = 'savegame'
 WORK_DIR = get_work_dir(NAME)
-logger = logging.getLogger(NAME)
 setup_logging(path=WORK_DIR, name=NAME)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='savegame',
-    version='2025.07.20.144450',
+    version='2025.07.20.144723',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/savegame',
